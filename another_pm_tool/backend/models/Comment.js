2985cd64b33351d4eefc7f2492eba56e4fdbd758
Utkantha <sutkanthapdreddy@gmail.com>
2026-09-24 22:04:23 +0530

    Remove duplicate project folder from repository

--- another_pm_tool/backend/models/Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CommentSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  task: { type: mongoose.Schema.Types.ObjectId, ref: 'Task', required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Comment', CommentSchema);
--- another_pm_tool/backend/models/List.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ListSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },
-  order: { type: Number, default: 0 },
-}, { timestamps: true });
-
-module.exports = mongoose.model('List', ListSchema);
